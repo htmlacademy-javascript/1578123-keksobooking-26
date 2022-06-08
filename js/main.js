0ca21499d4093508db3931c7b5a04dedf871c594
Anton <vakhania@opvf.ru>
2022-06-08 10:30:24 +0300

    Добавляет 2 функции генерации случайных чисел из диапазона

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 /* Вспомогательные функции для проекта */
 
+// Функция проверки на целое число
+const isInt = (num) => Number(num) === num && num % 1 === 0;
+
+// Функция проверки на число с плавающей точкой
+const isFloat = (num) => Number(num) === num && num % 1 !== 0;
+
 // Функция генерации случайных целых чисел в переданном диапазоне
 const getRandomInteger = (min, max) => {
   min = Math.abs(Math.ceil(Number(min)));
@@ -15,24 +21,29 @@ const getRandomInteger = (min, max) => {
 };
 
 // Функция генерации случайных целых чисел с плавающей точков (с указанным количеством знаков после запятой) в переданном диапазоне
-const getRandomFloat = (min, max, num) => {
-  min = Math.abs(min);
-  max = Math.abs(max);
+const getRandomFloat = (min, max, precision) => {
+  min = Math.abs(Number(min));
+  max = Math.abs(Number(max));
 
   if (!min || !max) {
     return 'Ошибка! Не переданы один или несколько аргументов функции!';
   }
 
   // Если не указано количество знаков после запятой, берём по умолчанию 2 знака
-  if (!num) {
-    num = 2;
+  if (!precision) {
+    precision = 2;
   }
 
-  const rand = min > max ? max + Math.random() * (min - max) :
-    min + Math.random() * (max - min);
+  let result = Math.round(Math.random() * Math.pow(10, precision)) / Math.pow(10, precision) * (max - min) + min;
 
-  return parseFloat(rand).toFixed(num);
+  if (min > max) {
+    result = Math.round(Math.random() * Math.pow(10, precision)) / Math.pow(10, precision) * (min - max) + max;
+  }
+
+  return parseFloat(result.toFixed(precision));
 };
 
+isInt(5);
+isFloat(1.24);
 getRandomInteger(0, 5);
 getRandomFloat(3, 7, 5);
